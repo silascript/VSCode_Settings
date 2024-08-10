9163bc52130b9d64c20bbecb70091c7d74bbd3f9
silascript <silascript@outlook.com>
2024-08-10 12:24:24 +0800

    完善读取扩展uid文件的函数

--- func_core.sh
+++ func_core.sh
@@ -12,8 +12,66 @@ function install_extension() {
 
 }
 
+# 读取扩展列表
+function read_extension_list() {
+
+	# 列表路径
+	local exlist_path=$1
+	# 扩展 uid 数组
+	local exuid_arr=()
+
+	# 过滤掉空行及使用#注释的行
+	for line in $(cat $exlist_path | grep -v ^$ | grep -v ^\#); do
+		# 把每行扩展的 uid 存储进数组中
+		exuid_arr+=($line)
+		# echo $line
+	done
+
+	# 返回 扩展uid 数组
+	# echo "${exuid_arr[@]}"
+
+	echo ${exuid_arr[@]}
+	# echo "${exuid_arr[*]}"
+	# echo $exuid_arr
+
+}
+
+# function test1(){
+# 	local array1=(2 5 22 32 18)
+#
+# 	# echo ${#array1[@]}
+#
+# 	echo ${array1[@]}
+# }
+
 ################################测试################################
 
 # 测试 install_extension 函数
 # install_extension "formulahendry.code-runner"
 # install_extension $1
+
+# --------------------------------------------------
+
+# 测试读取扩展列表函数
+arr_t1=($(read_extension_list $1))
+# read_extension_list $1
+
+# 查看数组元素个数
+echo ${#arr_t1[@]}
+
+# echo $arr_t1
+echo ${arr_t1[@]}
+
+echo "----------------------------"
+
+# 打印出数组元素
+for a_temp in ${arr_t1[@]}; do
+	echo $a_temp
+done
+
+# --------------------------------------------------
+# 测试 test1 函数
+# 获取返回值次构建为数组
+# r_arr=($(test1))
+# echo ${#r_arr[@]}
+# echo $r_arr
